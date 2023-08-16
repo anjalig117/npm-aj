50350ed8baf1f271e842223098b6e8faecf2d9a9
anjali <59136693+hp1317@users.noreply.github.com>
2023-08-16 22:56:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const QRCode = require('qrcode')
 const prompt = require("prompt-sync")({ sigint: true });
 
 function generateQr(){
-    const isQr = prompt("Do you want to create qr for the user details (y/n) ");
-
     let data = {
         name: 'Anjali Goel',
-        reach_me: 'anjaligoel117@gmail.com',
+        email: 'anjaligoel117@gmail.com',
         fun_fact: 'A kdrama fan',
         linkedIn: 'https://www.linkedin.com/in/anjali-goel-15043115a/',
         github: 'https://github.com/anjalig117'
     };
 
+    console.log(`${"-".repeat(101)}\nName: ${data.name}\nEmail: ${data.email}\nFun Fact: ${data.fun_fact}\nLinkedIn: ${data.linkedIn}\nGithub Profile: ${data.github}\n${"-".repeat(101)}`)
+
+    const isQr = prompt("Do you want to create qr for the user details? (y/n) ");
+
     let userData = JSON.stringify(data);
 
-    if (isQr == 'y'){
+    if (isQr.toLowerCase() == 'y'){
         QRCode.toString(userData, { type: 'terminal' },
         function (err, QRCode) {
             if (err) return console.log("error is ", err)
